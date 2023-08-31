c87cfa868adae72acb5e1975cebaa04ab6f0b2cd
Chandrasekaran Janardhanan <jjchandru@yahoo.com>
2023-08-31 15:46:03 +0530

    updated ui layout and intentionally failed the test case

--- spec/util.spec.mjs
+++ spec/util.spec.mjs
@@ -1,14 +1,12 @@
-import { displayTodayDate } from "../src/sample.js";
+import { displayTodayDate } from "../src/util.js";
 
 describe('first sample tests', () => {
-    it('should be true', () => {
-        expect(true).toBe(true);
-    });
     it('should display today date', () => {
         const element = document.createElement('div');
         element.setAttribute('id', 'date');
         document.body.appendChild(element);
         displayTodayDate();
         let date = new Date().toDateString();
+        expect(element.innerText).toEqual("incorrect date");
     });
 });
